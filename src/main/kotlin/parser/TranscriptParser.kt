d7ea1c9583c2458d83604907ac6fef5c2a9ab2a7
Petr Fuka <petr.fuka@gmail.com>
2025-01-08 20:29:27 +0100

    Better annotation detection

--- src/main/kotlin/parser/TranscriptParser.kt
+++ src/main/kotlin/parser/TranscriptParser.kt
@@ -251,7 +251,7 @@ private class TranscriptStripper : PDFTextStripper() {
 
 private val swornRegex = Regex("^(?<sworn>[^,\\p{Ll}]+), (?:Previously )?(?:Sworn|Affirmed|Acknowledges Oath), (?:Cross-e|E)xamined by .+\$")
 private val speakerRegex = Regex("^(?! )(?<speaker>[^\\p{Ll}]+:|Q|A)\\s+(?<text>.+)")
-private val parenthesisRegex = Regex("\\([^()]+\\)")
+private val parenthesisRegex = Regex("\\s*\\([^()]+\\)\\s*")
 
 /**
  * Check if the text is bold. Sometimes there is bold font on whitespace, that is a false positive for what we want
